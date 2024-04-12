cb375700e9772962906c5eb8a82a1ee4203f3074
Adeline <adelineeminian13@gmail.com>
2024-04-12 17:38:22 +0400

    final

--- client.c
+++ client.c
@@ -6,49 +6,56 @@
 /*   By: adel <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:16:25 by adel              #+#    #+#             */
-/*   Updated: 2024/03/22 15:56:06 by aeminian         ###   ########.fr       */
+/*   Updated: 2024/03/19 11:17:05 by adel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minitalk.h"
+
+void handler(int sig)
+{
+	if(sig == SIGUSR1)
+		printf("DONE\n");			
+}
+
 
 void	char_to_bin(int pid, char *str)
 {
 	int		i;
 	int		bit;
-
+	
 	i = 0;
 	while (str[i])
-	{
+	{	
 		bit = 0;
 		while (bit < 8)
 		{
-			if (str[i] >> bit & 1)
+			if(str[i] >> bit & 1)
 				kill(pid, SIGUSR1);
 			else if ((str[i] >> bit & 1) == 0)
 				kill(pid, SIGUSR2);
 			else
 			{
-				ft_printf("Error!");
+				write(1, "Error!", 7);
 				exit (1);
 			}
-			usleep(500);
+			usleep(2000);
 			bit++;
 		}
 		i++;
 	}
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	int	pid;
 
-	if (ac == 3)
+	signal(SIGUSR1, &handler);
+	if(ac == 3)
 	{
 		pid = ft_atoi(av[1]);
 		char_to_bin(pid, av[2]);
 		char_to_bin(pid, "\n");
 	}
 	else
-		ft_printf("Too few arguments.\n");
+		write(1, "Too few arguments.\n", 20);
 }
--- server.c
+++ server.c
@@ -1,57 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aeminian <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/22 16:02:59 by aeminian          #+#    #+#             */
-/*   Updated: 2024/03/22 16:03:25 by aeminian         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minitalk.h"
 
 void	handler(int sig, siginfo_t *info, void *ntg)
 {
     (void)ntg;
-    (void)info;
 	static int	bit = 0;
 	static int	i = 0;
-
 	if (sig == SIGUSR1)
 		i |= (0x01 << bit);
     else if (sig == SIGUSR2)
       i |= 0;
     bit++;
-	if (bit == 8)
-	{
-		ft_printf("%c", i);
+	    if (bit == 8)
+	{        
+		write(1, &i, 1);
 		i = 0;
-		bit = 0; 
+		bit = 0;
+        kill(info->si_pid, SIGUSR1);
 	}
 }
 int main(int ac, char **av)
 {
-	(void)av;
-	int pid;
-	struct sigaction sa;
-
-	pid = getpid();
-	if(ac == 1)
-	{
-		if (pid <= 0)
-		{
-			ft_printf("Failure!!!!");
-			exit(1);
-		}
-		ft_printf(">>>>>>>>>>CONNECTED TO 42POTATO'S SERVER:D<<<<<<<<<<<\nProcess ID : %d\n""Type...\n", pid);
-		sa.sa_flags = SA_SIGINFO;
-		sa.sa_sigaction = handler;
-		sigaction(SIGUSR1, &sa, NULL);
-		sigaction(SIGUSR2, &sa, NULL);
-		while (1)
-			pause();
-	}
-	return (0);
+    (void)av;
+    int pid;
+    struct sigaction sa;
+    
+    pid = getpid();
+    if(ac == 1)
+    {
+        if (pid <= 0)
+        {
+            write(1, "Failure!!!!", 12);
+            exit(1);
+        }
+        ft_printf(">>>>>>>>>>CONNECTED TO 42POTATO'S SERVER:D<<<<<<<<<<<\n");
+        sleep(1);
+        ft_printf("Process ID : %d\n", pid); 
+        sleep(1);
+        ft_printf("Type...\n");
+        sa.sa_flags = SA_SIGINFO;
+        sa.sa_sigaction = handler;
+        while (1)
+        {
+            sigaction(SIGUSR1, &sa, NULL);
+            sigaction(SIGUSR2, &sa, NULL);
+            pause();
+        }
+    }
+    else
+    {
+        write(1, "Error!\n", 8);
+        exit(1);
+    }
+    return (0);
 }
